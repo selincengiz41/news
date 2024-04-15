89d7ecf3b1d52142e35370464e8ea237555d363b
Selin Ayten Cengiz <selincengiz41@gmail.com>
2024-04-15 21:13:19 +0300

    Api key updated

--- app/src/main/java/com/selincengiz/news/common/Constants.kt
+++ app/src/main/java/com/selincengiz/news/common/Constants.kt
@@ -1,8 +1,11 @@
 package com.selincengiz.news.common
 
+import com.selincengiz.news.BuildConfig
+
+
 object Constants {
     const val BASE_URL = "https://newsdata.io/api/1/"
     const val GET_NEWS = "news"
-    const val API_KEY = "pub_39790ed6fbc11ccba280b796044f8bf0be469"
+   const val API_KEY = BuildConfig.API_KEY
 
 }
